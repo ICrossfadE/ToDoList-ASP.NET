3ed2fd75e5445c54cd81328a950c5ea94a58a419
Maks <Crossfade143@gmail.com>
2024-08-14 18:48:17 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/ToDoModel.cs
@@ -0,0 +1,8 @@
+namespace ToDoList.Models;
+
+public class ToDo
+{
+    public int? Id { get; set; }
+
+    public string? Name { get; set; }
+}
