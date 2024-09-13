6730cf7609e467a230bffe609bc3471b83257f80
Maks <Crossfade143@gmail.com>
2024-09-13 10:30:58 +0300

    status is done

--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8134592c7315ef29029716f769f8e62e7e9b81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a751c8cca65ebd62ec1457c449df5d27aaec8858")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
