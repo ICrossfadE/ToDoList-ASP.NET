2f00bfa8403573de4a8ef17ab1adcdf32d9bd6bd
Maks <Crossfade143@gmail.com>
2024-10-07 16:12:49 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ToDoList.Models; // додати простір імен з вашими моделями
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
